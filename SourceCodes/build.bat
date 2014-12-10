8d27772024a616ba493f81fb3d44205327f62bc2
Justin Yoo <justin.yoo@aliencube.com>
2014-12-10 21:53:39 +1100

    Add NuGet specification

---
+++ SourceCodes/build.bat
@@ -0,0 +1,35 @@
+@echo off
+
+reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath > nul 2>&1
+if ERRORLEVEL 1 goto MissingMSBuildRegistry
+
+for /f "skip=2 tokens=2,*" %%A in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath') do SET MSBUILDDIR=%%B
+
+IF NOT EXIST %MSBUILDDIR%nul goto MissingMSBuildToolsPath
+IF NOT EXIST %MSBUILDDIR%msbuild.exe goto MissingMSBuildExe
+
+::BUILD
+"tools\nuget.exe" restore EnumConverter.sln 
+"%MSBUILDDIR%msbuild.exe" "02_Apps\EnumConverter\EnumConverter.csproj" /t:ReBuild /p:Configuration=Release;TargetFrameworkVersion=v4.5;DefineConstants="TRACE;NET45";OutPutPath=bin\Release\net45\;DocumentationFile=bin\Release\net45\EnumConverter.xml
+
+mkdir build
+del "build\*.nupkg"
+
+::PACK
+"tools\nuget.exe" pack "02_Apps\EnumConverter\EnumConverter.nuspec" -OutputDirectory build
+
+::DEPLOY
+"tools\nuget.exe" push "build\*.nupkg"
+
+goto:eof
+::ERRORS
+::---------------------
+:MissingMSBuildRegistry
+echo Cannot obtain path to MSBuild tools from registry
+goto:eof
+:MissingMSBuildToolsPath
+echo The MSBuild tools path from the registry '%MSBUILDDIR%' does not exist
+goto:eof
+:MissingMSBuildExe
+echo The MSBuild executable could not be found at '%MSBUILDDIR%'
+goto:eof
